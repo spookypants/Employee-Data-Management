a2665775bedead0bfbe7f01d788291b9c2a184ad
Gabe <42557432+spookypants@users.noreply.github.com>
2019-06-12 12:02:36 -0500

    added firebase API key

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBTemqNSEfdp9LrPc2uB5Oa_RKVwquc46s",
+    authDomain: "employee-tracker-aa369.firebaseapp.com",
+    databaseURL: "https://employee-tracker-aa369.firebaseio.com",
+    projectId: "employee-tracker-aa369",
+    storageBucket: "",
+    messagingSenderId: "51820322465",
+    appId: "1:51820322465:web:c87e94b710c01d1e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
